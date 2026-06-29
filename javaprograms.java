83e815b33a5831401bfab796bd2420feeaf05020
dhanasrisai <dhanusreeramdas@gmail.com>
2026-06-29 16:33:00 +0530

    addition program

---
+++ javaprograms.java
@@ -0,0 +1,12 @@
+public class Addition
+{
+    public static void main(String args[])
+    {
+        void add(int a, int b)
+        {
+            int c=a+b
+            System.out,println("Additon of two number is :" c)
+        }
+        add(20,10);
+    }
+}
