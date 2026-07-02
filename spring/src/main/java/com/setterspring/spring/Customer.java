3c6c926e5878f6d6bf563da15e86c55fb8992aad
dhanasrisai <dhanusreeramdas@gmail.com>
2026-07-02 10:27:10 +0530

    spring

---
+++ spring/src/main/java/com/setterspring/spring/Customer.java
@@ -0,0 +1,28 @@
+package com.setterspring.spring;
+
+public class Customer
+{
+   private String name;
+   private long number;
+   private String address;
+   
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public long getNumber() {
+	return number;
+}
+public void setNumber(long number) {
+	this.number = number;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+   
+}
