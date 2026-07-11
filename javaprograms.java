7df9cf62c4918ce4f525c132550510e1ba85e6e0
dhanasrisai <dhanusreeramdas@gmail.com>
2026-07-11 10:00:21 +0530

    add another addititon value

--- javaprograms.java
+++ javaprograms.java
@@ -3,11 +3,12 @@ public class javaprograms
     void add(int a, int b)
         {
             int c=a+b;
-            System.out.println("Additon of two number is :"+c);
+            System.out.println("Addition of two number is :"+c);
         }
     public static void main(String args[])
     {
         javaprograms ad=new javaprograms();
         ad.add(20,10);
+        ad.add(30,40);
     }
 }
